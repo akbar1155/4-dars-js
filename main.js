33fdf05e49b460e3612c7532014b2024d6e45d2b
Akbar <isroilovakbar49@gmail.com>
2023-01-18 21:26:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,59 @@
+// let n = [7, 2, 4, 5];
+// let x = 9
+
+
+// for (let i = 0; i <= 4; i++) {
+//     x = x + i
+//     if (x = 9) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+
+// }
+
+
+
+
+// x = 0
+// while (n[i]) {
+//     x = x + i
+//     if (x = 9) {
+//         console.log("true");
+//     }
+// }
+
+
+// let age = 10
+
+// let res = age > 17 ? "sddasdas" : "adsasdasdsfsfsdfgdsafaffdasfda"
+
+// let age = 12
+// let res1 = age > 18 ? "pubg" : age > 15 ? "dota" : age > 10 ? "cs" : chess
+
+// console.log(res);
+// console.log(res1);
+
+
+
+
+// let n = [7, 2, 4, 5];
+// let result = 0; 
+// for(let i = 0; i < n.length; i++){
+//     result += n[i];
+// }
+// console.log(result);
+
+
+
+
+
+
+
+let a=prompt("son kirit")
+
+
+
+
+let res = a >= 10 ? a * 90 : a > 5 ? a * 95 : a <= 5 ? a * 100 : false
+console.log(res);
